7ef799f59e17a6bbfdfaaa3e4edbb614da1e5f58
Vu Tung Lam <vutunglaminfo@gmail.com>
2026-04-18 13:50:49 +0700

    feat: communicating with driver (async) ; fix service long stop time

--- main/src/WindowsServiceImpl.cpp
+++ main/src/WindowsServiceImpl.cpp
@@ -2,6 +2,8 @@
 #include "WindowsServiceImpl.h"
 #include "ThreadPool.h"
 #include "Logging.h"
+#include "utils.h"
+#include "pmd_driver/driver.h"
 #pragma endregion
 
 CWindowsServiceImpl::CWindowsServiceImpl(PWSTR pszServiceName, 
@@ -18,12 +20,25 @@ CWindowsServiceImpl::CWindowsServiceImpl(PWSTR pszServiceName,
     {
         throw GetLastError();
     }
+
+    // Event to notify stopping to the worker thread
+    m_hStoppingEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    if (m_hStoppingEvent == NULL)
+    {
+        throw GetLastError();
+    }
 }
 
 
 CWindowsServiceImpl::~CWindowsServiceImpl(void)
 {
-	if (m_hStoppedEvent)
+    if (m_hStoppingEvent)
+    {
+        CloseHandle(m_hStoppingEvent);
+        m_hStoppingEvent = NULL;
+    }
+
+    if (m_hStoppedEvent)
     {
         CloseHandle(m_hStoppedEvent);
         m_hStoppedEvent = NULL;
@@ -44,18 +59,63 @@ void CWindowsServiceImpl::OnStart(DWORD dwArgc, LPWSTR *lpszArgv)
 
 void CWindowsServiceImpl::ServiceWorkerThread(void)
 {
+    PENDING_SCAN_TASK pendingTask = { 0 };
+
     DebugLog(L"Service worker thread is running.");
 
-    // Periodically check if the service is stopping.
+    HANDLE hDriver = OpenDriverDevice();
+    while (hDriver == INVALID_HANDLE_VALUE || hDriver == NULL) {
+        DebugLog(L"Failed to open driver device. Retrying in 10 seconds.");
+        WaitForMultipleObjects(1, &m_hStoppingEvent, FALSE, 10000);
+        if (m_fStopping) {
+            DebugLog(L"Service worker thread is stopping during driver open retry.");
+            goto cleanup;
+        }
+        HANDLE hDriver = OpenDriverDevice();
+    }
+    DebugLog(L"Driver device opened successfully.");
+
+    if (!RegisterWithDriver(hDriver)) {
+        DebugLog(L"Failed to register with driver. Exiting service worker thread.");
+        goto cleanup;
+    }
+
+    HANDLE events[2] = { m_hStoppingEvent, NULL };
     while (!m_fStopping)
     {
-        // Perform main service function here...
+        DebugLog(L"Calling GetPendingTaskAsync...");
+        if (!GetPendingTaskAsync(hDriver, &pendingTask)) {
+            DebugLog(L"GetPendingTaskAsync failed.");
+            break;
+        }
+        events[1] = pendingTask.ov.hEvent;
 
-        ::Sleep(2000);  // Simulate some lengthy operations.
+        DebugLog(L"Waiting for either stopping event or pending task...");
+        DWORD w = WaitForMultipleObjects(2, events, FALSE, INFINITE);
+
+        DebugLog(L"Woke up from wait with result: ", w);
+        if (w == WAIT_OBJECT_0) {
+            CancelPendingTask(hDriver, &pendingTask);
+            break;
+        } else {
+            SCAN_TASK_DTO task = { 0 };
+            if (!CompletePendingTask(hDriver, &pendingTask, &task)) {
+                DebugLog(L"CompletePendingTask failed.");
+                break;
+            }
+
+            std::wstring filePath = FileIdToPath(task.FileId, task.VolumeSerialNumber);
+
+            DebugLog(L"Scanning file: ", filePath, " ( PID: ", (ULONG_PTR)task.Pid, L" )");
+        }
     }
 
     // Signal the stopped event.
+cleanup:
     DebugLog(L"Service worker thread is stopping.");
+    if (hDriver != INVALID_HANDLE_VALUE && hDriver != NULL) {
+        CloseHandle(hDriver);
+    }
     SetEvent(m_hStoppedEvent);
 }
 
@@ -69,6 +129,7 @@ void CWindowsServiceImpl::OnStop()
     // Indicate that the service is stopping and wait for the finish of the 
     // main service function (ServiceWorkerThread).
     m_fStopping = TRUE;
+    SetEvent(m_hStoppingEvent);
     if (WaitForSingleObject(m_hStoppedEvent, INFINITE) != WAIT_OBJECT_0)
     {
         throw GetLastError();
